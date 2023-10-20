0b34b1950a88733618b049f10c4a8492140d475a
Ted Klingenberg <ted@doseofted.com>
2023-10-19 23:07:52 -0400

    Fix for auto-reload in dev mode

--- client/index.ts
+++ client/index.ts
@@ -1,11 +1,10 @@
 import { client } from "./prim"
 
+// NOTE: ignore this line: it's just for development mode
+if (import.meta.env.DEV) fetch("http://localhost:3001/prim").catch(() => setTimeout(() => location.reload(), 300))
+
 const greeting = await client.sayHello()
 console.log(greeting) // "Frontend, meet Backend."
 
 const app = document.getElementById("app")
 if (app) app.innerText = greeting
-
-// NOTE: ignore this bit: it's just here for development mode
-const delayReload = () => setTimeout(location.reload, 300)
-if (import.meta.env.DEV) fetch("http://localhost:3000/prim").catch(delayReload)
